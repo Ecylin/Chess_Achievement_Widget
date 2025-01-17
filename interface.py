6dd8c729c47b0cc541d61dc6b061288a6b557604
Ecylin <106186762+Ecylin@users.noreply.github.com>
2025-01-17 15:20:25 -0500

    Update interface.py
    Modified interface to list achievements in multiple as needed

--- interface.py
+++ interface.py
@@ -9,25 +9,48 @@ root.geometry("600x400")
 username_var = tk.StringVar()
 
 
+# function pulled from https://stackoverflow.com/questions/171662/formatting-a-list-of-text-into-columns
+def formatcols(mylist, cols):
+    maxWidth = max(map(lambda x: len(x), mylist))
+    justifyList = list(map(lambda x: x.ljust(maxWidth), mylist))
+    lines = (' '.join(justifyList[i:i + cols])
+             for i in range(0, len(justifyList), cols))
+    return "\n".join(lines)
+
+
+def format_list(input_list, maxsize):
+    size = len(input_list.splitlines())
+    return formatcols(input_list.splitlines(), size // maxsize + 1)
+
+
 def submit():
     username = username_var.get()
 
     try:
-        achievement_list = f"Missing Achievements:\n{get_names(get_missing(username))}"
+        achievement_list = f"{get_names(get_missing(username))}"
+        output = format_list(achievement_list, 40)
+
+        print(output)
+
     except KeyError:
-        achievement_list = f"Unable to load achievements for user \"{username}\"." \
-                           "\nMake sure you input your username correctly"
+        output = f"Unable to load achievements for user \"{username}\"." \
+                 "\nMake sure you input your username correctly"
 
+    missing_names_head = tk.Label(root,
+                                  text=f"Missing Achievements for \"{username}\":",
+                                  font=('Courier', 12))
     missing_names = tk.Label(root,
-                             text=achievement_list,
-                             font=('calibre', 12))
+                             text=output,
+                             font=('Courier', 10),
+                             justify="left")
 
-    missing_names.grid(row=2, column=0, columnspan=3)
+    missing_names_head.grid(row=2, column=0, columnspan=5)
+    missing_names.grid(row=3, column=0, columnspan=5)
 
 
 # creating a label for
 # name using widget Label
-username_label = tk.Label(root, text='Username', font=('calibre', 10, 'bold'))
+username_label = tk.Label(root, text='Username', font=('calibre', 12))
 
 # creating an entry for input
 # name using widget Entry
